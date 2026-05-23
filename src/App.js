be4619688df68b512ade60339f4258cf3957de47
Peter Jakes <kiberujakes@gmail.com>
2026-05-23 16:26:10 +0300

    feat: wired up all sections in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import TechStack from './components/TechStack';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-black">
+      <Navbar />
+      <Hero />
+      <About />
+      <TechStack />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
